5d1ad3b8eb2785e4e2084d1635dc101a56bc27ab
ltran505 <124006655+ltran505@users.noreply.github.com>
2024-06-20 16:53:53 -0700

    Create lab_1.cpp

---
+++ lab_1.cpp
@@ -0,0 +1,16 @@
+Include your name (if a team, include team leader AND other members):
+// Duck, Donald (Team Leader)
+// Mouse, Mickey
+// Duck, Daisy
+// ...
+
+
+// Part 1
+Your code for part 1 goes here...
+
+
+// Part 2
+Your code for part 2 goes here...
+
+
+And so on...
