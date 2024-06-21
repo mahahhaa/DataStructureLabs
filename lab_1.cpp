c3e5cc6e644b45c904f575dc3d0f4c85cfdd7288
ltran505 <124006655+ltran505@users.noreply.github.com>
2024-06-20 17:08:12 -0700

    Update lab_1.cpp

--- lab_1.cpp
+++ lab_1.cpp
@@ -1,16 +1,74 @@
-Include your name (if a team, include team leader AND other members):
-// Duck, Donald (Team Leader)
+// Tran, Lily (Team Leader)
 // Mouse, Mickey
-// Duck, Daisy
-// ...
-
 
 // Part 1
-Your code for part 1 goes here...
-
+map<string, set<string> > neighbors{
+  { "Illinois", {"Indiana", "Iowa", "Michigan", "Missouri", "Wisconsin"} },
+  { "Indiana", {"Illinois", "Michigan", "Ohio"} },
+  { "Iowa", {"Illinois", "Minnesota", "Missouri", "Nebraska", "South Dakota", "Wisconsin" } },
+  { "Kansas", {"Missouri", "Nebraska" } },
+  { "Michigan", {"Illinois", "Indiana", "Minnesota", "Ohio", "Wisconsin" } },
+  { "Minnesota", {"Iowa", "Michigan", "North Dakota", "South Dakota", "Wisconsin" } },
+  { "Missouri", {"Illinois", "Iowa", "Kansas", "Nebraska" } },
+  { "Nebraska", {"Iowa", "Kansas", "Missouri", "South Dakota" } },
+  { "North Dakota", {"Minnesota", "South Dakota" } },
+  { "Ohio", {"Indiana", "Michigan" } },
+  { "South Dakota", {"Iowa", "Minnesota", "Nebraska", "North Dakota" } },
+  { "Wisconsin", {"Illinois", "Iowa", "Michigan", "Minnesota" } },
+};
 
 // Part 2
-Your code for part 2 goes here...
+void printNeighbors(const map<string, set<string>>& neighbors)
+{
+   for(auto i : neighbors)
+   {
+      cout << i.first << " borders with: ";
+      for(const string& j : i.second)
+      {
+         cout << j << " ";
+      }
+      cout << endl;
+   }
+}
 
+// Part 3
+vector<vector<int>> neighbors{
+  {0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0 , 1},
+  {1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 , 0},
+  {1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1 , 1},
+  {0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0 , 0},
+  {1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0 , 1},
+  {0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1 , 1},
+  {1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0 , 0},
+  {0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1 , 0},
+  {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 , 0},
+  {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0 , 0},
+  {0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0 , 0},
+  {1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0 , 0},
+};
 
-And so on...
+// Part 4
+bool areNeighbors(const vector<string>& states, const vector<vector<int>>& neighbors, 
+   const string& state1, const string& state2)
+{
+   int i = 0, row = 0, column = 0;
+   bool foundRow = false, foundColumn = false;
+   
+   while (!foundRow || !foundColumn)
+   {
+      if(states[i] == state1) 
+      {
+         row = i;
+         foundRow = true;
+      }
+      else if(states[i] == state2) 
+      {
+         column = i;
+         foundColumn = true;
+      }
+      
+      ++i;
+   }
+   
+   return (neighbors[row][column] == 1);
+}
